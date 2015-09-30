e19394361defb73107c29808ee0300b0cbb9829e
Arunkumar <b.arun410@gmail.com>
2015-09-30 13:21:25 -0400

    With Feed class and AsyncTask to initiate retrieving feeds.

---
+++ Group6A_HW04/app/src/main/java/com/group6a_hw04/group6a_hw04/Feed.java
@@ -0,0 +1,80 @@
+package com.group6a_hw04.group6a_hw04;
+
+/**
+ * Created by Arunkumar's on 9/30/2015.
+ */
+public class Feed {
+    String ftitle,flargeImage,fartist,fduration,fcategory,freleaseDate,fsummary,fprice,flinkToPreview;
+
+    public String getFtitle() {
+        return ftitle;
+    }
+
+    public void setFtitle(String ftitle) {
+        this.ftitle = ftitle;
+    }
+
+    public String getFlargeImage() {
+        return flargeImage;
+    }
+
+    public void setFlargeImage(String flargeImage) {
+        this.flargeImage = flargeImage;
+    }
+
+    public String getFartist() {
+        return fartist;
+    }
+
+    public void setFartist(String fartist) {
+        this.fartist = fartist;
+    }
+
+    public String getFduration() {
+        return fduration;
+    }
+
+    public void setFduration(String fduration) {
+        this.fduration = fduration;
+    }
+
+    public String getFcategory() {
+        return fcategory;
+    }
+
+    public void setFcategory(String fcategory) {
+        this.fcategory = fcategory;
+    }
+
+    public String getFreleaseDate() {
+        return freleaseDate;
+    }
+
+    public void setFreleaseDate(String freleaseDate) {
+        this.freleaseDate = freleaseDate;
+    }
+
+    public String getFsummary() {
+        return fsummary;
+    }
+
+    public void setFsummary(String fsummary) {
+        this.fsummary = fsummary;
+    }
+
+    public String getFprice() {
+        return fprice;
+    }
+
+    public void setFprice(String fprice) {
+        this.fprice = fprice;
+    }
+
+    public String getFlinkToPreview() {
+        return flinkToPreview;
+    }
+
+    public void setFlinkToPreview(String flinkToPreview) {
+        this.flinkToPreview = flinkToPreview;
+    }
+}
