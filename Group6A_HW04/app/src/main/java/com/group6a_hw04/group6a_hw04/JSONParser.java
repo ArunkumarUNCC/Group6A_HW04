0d9e2ff9d14a3a55ef06a7eccb7ca088b0b522bc
Arunkumar <b.arun410@gmail.com>
2015-09-30 13:29:25 -0400

    With class JSONParser to parse the obtained feeds...

---
+++ Group6A_HW04/app/src/main/java/com/group6a_hw04/group6a_hw04/JSONParser.java
@@ -0,0 +1,15 @@
+package com.group6a_hw04.group6a_hw04;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Arunkumar's on 9/30/2015.
+ */
+public class JSONParser {
+    public static class ParseAppFeeds{
+        static ArrayList<Feed> parseFeeds(String astringBuilder){
+
+            return null;
+        }
+    }
+}
